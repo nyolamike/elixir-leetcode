d736963ded35db1fa01f05eb4199fe75fdc21fc3
Nyola Mike a.k.a gr33ndot <iamnyola@gmail.com>
2022-02-26 08:34:13 +0300

    disappeared_numbers

---
+++ lib/t448_easy_find-all-numbers-disappeared-in-an-array.ex/solution.ex
@@ -0,0 +1,30 @@
+defmodule Solution do
+  @spec find_disappeared_numbers(nums :: [integer]) :: [integer]
+  def find_disappeared_numbers(nums) do
+      #the length of an the list is n
+      #so we go through the list but checking for number that are below or equal to n
+      #but these numbers are not in the list
+
+      #we need to reduce
+      tally = {
+        %{},
+        0
+      }
+      {checks, len} = nums |> Enum.reduce(tally, fn num, {checks, len} ->
+         len = len + 1
+         checks = checks |> put_item(num) |> put_item(len,0)
+         {checks, len}
+      end)
+      #get keys with zero count
+      checks |> Enum.reduce([], fn {key, value}, acc ->
+        if value == 0, do: [key | acc], else: acc
+      end)
+  end
+  def put_item(checks, num, increment \\ 1) do
+      if checks |> Map.has_key?(num) == false do
+          checks |> Map.put(num, increment)
+      else
+          checks |> Map.put(num, checks[num] + increment)
+      end
+  end
+end
